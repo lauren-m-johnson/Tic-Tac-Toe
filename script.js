9f6d3d96acd7380fe41e3bbef1a938e06f62d0ff
lauren-m-johnson <laurenmj90@gmail.com>
2023-06-09 20:29:27 -0700

    create renderboard() function and styled player selections

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 /*----- constants -----*/
 const MARK = {
-    'x' : 'X',
-    'o' : 'O'
+    '1' : 'X',
+    '2': 'O',
+    '0': null
 }
 
 /*----- state variables -----*/
@@ -10,7 +11,7 @@ let turn; // x or o
 let winner; //null, o or x winner, 'T' = tie
 
 /*----- cached elements  -----*/
-
+const msgEl = document.querySelector('h1');
 
 /*----- event listeners -----*/
 
@@ -24,11 +25,37 @@ function init() {
         [0, 0, 0],
         [0, 0, 0]
     ];
-    turn = 'x';
+    turn = 1;
     winner = null;
     render();
 }
 
 function render() {
+    renderBoard();
+    renderMessage();
+    renderControls();
+}
+
+function renderBoard() {
+    board.forEach(function(colArr, colIdx) {
+       colArr.forEach(function(cellVal, rowIdx) {
+        const cellId = `c${colIdx}r${rowIdx}`;
+        const cellEl = document.getElementById(cellId);
+        cellEl.innerHTML = MARK[cellVal]
+        //styles for MARK
+        cellEl.style.fontSize = '19vmin';
+        cellEl.style.color = 'orchid';
+        cellEl.style.display = 'flex';
+        cellEl.style.justifyContent = 'center';
+        cellEl.style.alignItems = 'center'; 
+       }) 
+    });
+}
+
+function renderMessage() {
     
+}
+
+function renderControls() {
+
 }
