596c1ce0a254abbd33224e4a4fbd584460e07f0b
lauren-m-johnson <laurenmj90@gmail.com>
2023-06-11 12:02:00 -0700

    created getWinner() function

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 /*----- constants -----*/
+//Display of background color for the selected box. Player 1 is 1, pink and X. Player 2 is -1, green and O
 const COLORS = {
     '0': 'white',
     '1': 'pink',
     '-1': 'lightgreen'
 };
 
+//Display for selected box. X or O
 const MARK = {
     '0': '',
     '1': 'X',
     '-1': 'O'
-}
+};
+
+//Winning combos to win the math
+const COMBOS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [6, 4, 2],
+    [0, 4, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+];
 
 /*----- state variables -----*/
 let board; //array of 9 boxes
 let turn; // 1 or -1
-let winner; //null = no winner; 1 or -1 winner; 'T'
+let winner; //null = no winner; 1 or -1 winner; 'T' = Tie
 
 /*----- cached elements  -----*/
 const message = document.querySelector('h1');
@@ -55,10 +69,10 @@ function renderBoard() {
     });
 }
 
-//Displays whose turn it is and the winner
+//Display whose turn it is and the winner
 function renderMessage() {
     if (winner === 'T') {
-        message.innerHTML = 'Tie Game!';
+        message.innerHTML = 'Tie Game! Game Over!';
     } else if (winner) {
         message.innerHTML = `Player ${MARK[winner]} Wins!`;
     } else {
@@ -66,8 +80,8 @@ function renderMessage() {
     }
 }
 
+//Get index of the clicked box
 function handleClick(event) {
-    //gets index of the clicked box
     const boxIdx = parseInt(event.target.id.replace('box-', ''));
     //if statement in case someone clicks outside box, the box is filled or there is a winner
     if ( isNaN(boxIdx) || board[boxIdx] || winner )
@@ -77,10 +91,21 @@ function handleClick(event) {
     //switch player turn
     turn *= -1;
     // check for a winner
-    winner = displayWinner();
+    winner = getWinner();
     render();
 }   
 
-function displayWinner() {
-
+//Check for a winner in the state. 1(X) or -1(O), 'T' for Tie, null for no winner yet
+//Got really stuck on this section. Had to peak at the solution and research Math.abs function
+function getWinner() {
+    for (let i = 0; i < COMBOS.length; i++) {
+        if (Math.abs(board[COMBOS[i][0]] + board[COMBOS[i][1]] + board[COMBOS[i][2]]) === 3) {
+            return board[COMBOS[i][0]];
+        } else if (board.includes(null)) {
+            return null;
+        }
+    }
+    //return 'T'; 
+    //When I impliment this, the game ends after just one move. I don't understand why it is bi-passing the loop. 
+    //When I put the statement inside the loop, it also doesn't work.
 }
